4051751a932ba9b8103303f01204290246d23a38
Elco Bakker <e.bakker@sms.ed.ac.uk>
2015-07-09 14:41:42 +0100

    improvement to stacks code so it doesn’t pad arrays and is faster.

--- GetSubStack.m
+++ GetSubStack.m
@@ -18,41 +18,44 @@ end
 if nargin<4
     PadImage = mean(Stack(:));
 else
-    PadImage = logical(varargin{1});
+    PadImage = varargin{1};
 end
 
 
-    Stack = padarray(Stack,SizeOfSubStack,PadImage);
+    %Stack = padarray(Stack,SizeOfSubStack,PadImage);
 
     extent = ceil(SizeOfSubStack/2);
     range = cell(length(SizeOfSubStack),1);
- 
+    corresponding_range = range;
+    
     for centrei = 1:size(Centres,1)
         
-        ReturnStack = true;
-        
+
         for dimensioni =1:length(SizeOfSubStack)
             if dimensioni<=size(Centres,2)
-                range{dimensioni} = Centres(centrei,dimensioni) - extent(dimensioni)  + (1:SizeOfSubStack(dimensioni)) +SizeOfSubStack(dimensioni);
+                range{dimensioni} = Centres(centrei,dimensioni) - extent(dimensioni)  + (1:SizeOfSubStack(dimensioni));
                 %centres for odd sizes
                 %for even sizes(not recommended) stack is up down from 'true'
                 %fractional centre.
             else
                 
-                range{dimensioni} = ceil(StackSize(dimensioni)/2) - extent(dimensioni) + (1:SizeOfSubStack(dimensioni)) + +SizeOfSubStack(dimensioni);
+                range{dimensioni} = ceil(StackSize(dimensioni)/2) - extent(dimensioni) + (1:SizeOfSubStack(dimensioni)) ;
             end
                 
-            if range{dimensioni}(1)<1 || range{dimensioni}(end)>(StackSize(dimensioni) + 2*+SizeOfSubStack(dimensioni))
-                ReturnStack = false;
-            end
-
+            corresponding_range{dimensioni} = 1:SizeOfSubStack(dimensioni);
         
+            good_values = range{dimensioni}>1 & range{dimensioni}<=StackSize(dimensioni);
+            
+            range{dimensioni} = range{dimensioni}(good_values);
+            corresponding_range{dimensioni} = corresponding_range{dimensioni}(good_values);
         end
         
-        if ReturnStack
-            
-            SubStackCell{centrei} = Stack(range{1},range{2},range{3});
-            
+        if length(StackSize)==3
+            SubStackCell{centrei} = PadImage*ones(SizeOfSubStack);
+            SubStackCell{centrei}(corresponding_range{1},corresponding_range{2},corresponding_range{3}) = Stack(range{1},range{2},range{3});
+        elseif length(StackSize) ==2
+            SubStackCell{centrei} = PadImage*ones(SizeOfSubStack);
+            SubStackCell{centrei}(corresponding_range{1},corresponding_range{2}) = Stack(range{1},range{2});
         end
         
     end
