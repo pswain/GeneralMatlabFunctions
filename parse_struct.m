1aaf8ad8d68eff76d13dd01114fd233e0797517e
Elco Bakker <e.bakker@sms.ed.ac.uk>
2017-10-20 17:25:54 +0100

    some random additions and an important modification to parse_struct.

--- parse_struct.m
+++ parse_struct.m
@@ -1,12 +1,22 @@
 function output_struct = parse_struct(input_struct,default_struct)
 % output_struct = parse_struct(input_struct,default_struct)
 %
-% replaces any field in default struct with the one present in input struct
+% replaces any field in default_struct with the one present in input_struct
 % and then returns the modified default_struct.
+%
+% if the field is itself a structure, and the correpsonding field in
+% defeault_struct is a struct it calls parse_struct on these fields.
 output_struct = default_struct;
 fields = fieldnames(input_struct);
-for fi = 1:length(fields);
-    output_struct.(fields{fi}) = input_struct.(fields{fi});
+for fi = 1:length(fields)
+    if isstruct(input_struct.(fields{fi})) ...
+            && isfield(output_struct,fields{fi})...
+            && isstruct(output_struct.(fields{fi}))
+        output_struct.(fields{fi}) = ...
+            parse_struct(input_struct.(fields{fi}),output_struct.(fields{fi}));
+    else
+        output_struct.(fields{fi}) = input_struct.(fields{fi});
+    end
 end
 
 end
